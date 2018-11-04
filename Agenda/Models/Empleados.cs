bb76cc25268120210adf1fff5802ead2b5dc704c
joshaldair <joshaldair@gmail.com>
2018-11-04 17:25:29 -0500

    Se crea la clase de empleado

---
+++ Agenda/Models/Empleados.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agenda.Models
+{
+    public class Empleados
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Office { get; set; }
+        public string Salary { get; set; }
+    }
+}
